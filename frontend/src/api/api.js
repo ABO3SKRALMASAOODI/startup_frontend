e1e0acfc53d4570c0d2521d3027ed1e2857c195d
MUSLIM <muslimtariq0@gmail.com>
2026-03-07 19:38:36 +0400

    proxy backend through Vercel to fix CORS

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://entrepreneur-bot-backend.onrender.com",
+  baseURL: "/api-backend",
 });
 
 API.interceptors.request.use((config) => {
