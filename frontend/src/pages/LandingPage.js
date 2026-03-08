d15e1c94d04fbdf10f65aa4144f45a4251dcd65b
MUSLIM <muslimtariq0@gmail.com>
2026-03-09 00:39:11 +0400

    Move robot higher - head behind title

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -358,7 +358,7 @@ function LandingPage() {
             position: "absolute",
             top: "50%",
             left: "50%",
-            transform: "translate(-50%, -18%)",
+            transform: "translate(-50%, -85%)",
             width: "700px",
             height: "700px",
             zIndex: 1,
